ffaccc633bcff8ae03d8293d3ac012ca2507291f
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-26 19:54:29 +0300

    Commit exercise 4.3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,6 +1,6 @@
 const dummy = require('../utils/list_helper').dummy
 
-test('Dummy test', () => {
+test('Dummy returns 1', () => {
   const blogs = []
   const result = dummy(blogs)
   expect(result).toBe(1)

2d3dc157c5d9abf8022d0696b0f375b55e613a56
InstaFall <gorkemcanakilli@hotmail.com>
2022-10-26 20:20:35 +0300

    Commit exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
+const dummy = (blogs) => { // eslint-disable-line no-unused-vars
   return 1
 }
 
+const totalLikes = (blogs) => {
+  //if (blogs.length > 0) {
+  let sum = 0
+  for (const blog of blogs){
+    sum += blog.likes
+  }
+  return sum
+  //} else return 0
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
